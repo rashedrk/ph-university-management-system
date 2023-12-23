37c8db1d7ed571fd548ef0fceea9f5975861bde2
rashedrk <rashedrk001@gmail.com>
2023-12-23 12:50:17 +0600

    catch aysnc refactor in utils folder

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,25 +1,22 @@
+import catchAsync from "../../utils/catchAsync";
 import sendResponse from "../../utils/sendResponse";
 import { UserServices } from "./user.service";
-import { NextFunction, Request, Response } from 'express';
 
-const createStudent = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const { password, student: studentData } = req.body;
+const createStudent = catchAsync(async (req, res) => {
+    const { password, student: studentData } = req.body;
 
-        //validation using zod
-        // const zodParsedData = studentValidationSchema.parse(studentData);
+    //validation using zod
+    // const zodParsedData = studentValidationSchema.parse(studentData);
 
-        //call service func to send this data
-        const result = await UserServices.createStudentIntoDB(password, studentData);
+    //call service func to send this data
+    const result = await UserServices.createStudentIntoDB(password, studentData);
 
-        sendResponse(res, {
-            message: 'Student is created successfully',
-            data: result,
-        });
-    } catch (err) {
-        next(err);
-    }
-};
+    sendResponse(res, {
+        message: 'Student is created successfully',
+        data: result,
+    });
+
+});
 
 export const UserControllers = {
     createStudent,
