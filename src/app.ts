74b577fea51b739524af46df6b6d6767aa3b453e
rashedrk <rashedrk001@gmail.com>
2023-12-23 12:14:01 +0600

    Create index route

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import { StudentRoutes } from './app/modules/student/student.route';
-import { UserRoutes } from './app/modules/user/user.route';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFoundError from './app/middlewares/notFoundError';
+import router from './app/routes';
 const app: Application = express();
 
 //parsers
@@ -11,8 +10,8 @@ app.use(express.json());
 app.use(cors());
 
 //application routes
-app.use('/api/v1/students', StudentRoutes);
-app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1', router);
+
 
 app.get('/', (req: Request, res: Response) => {
   const a = 10;
